338b051d7faf5a74b295a37253bd1f246cf022e8
Anatolii Shumakov <a.shumakoff@protonmail.com>
2021-09-07 14:58:26 +0500

    Changes for Survey ViewSet

--- survey/core/views.py
+++ survey/core/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from datetime import date
+from django.shortcuts import render, get_object_or_404
 from rest_framework import viewsets
 from rest_framework import status
 from rest_framework.response import Response
@@ -6,12 +7,54 @@ from core.models import Survey, QuestionType, Question, Choice
 from core.serializers import SurveySerializer, QuestionTypeSerializer, QuestionSerializer, ChoiceSerializer
 
 
-class SurveyViewSet(viewsets.ModelViewSet):
+class SurveyViewSet(viewsets.ViewSet):
     """
-    API endpoint that allows Surveys to be viewed.
+    API endpoint that allows Surveys to be managed
     """
-    queryset = Survey.objects.all()
-    serializer_class = SurveySerializer
+
+
+    def list(self, request):
+        queryset = Survey.objects.all()
+        serializer = SurveySerializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+    def retrieve(self, request, pk):
+        survey = get_object_or_404(Survey.objects.all(), id=pk)
+        serializer = SurveySerializer(survey)
+        return Response(serializer.data)
+
+
+    def create(self, request):
+        serializer = SurveySerializer(data=request.data, many=True)
+        if serializer.is_valid():
+            serializer.save()
+        else:
+            return Response(serializer.errors,
+                            status=status.HTTP_400_BAD_REQUEST)
+
+
+    def update(self, request, pk):
+        survey = get_object_or_404(Survey.objects.all(), id=pk)
+        serializer = SurveySerializer(data=request.data)
+        if serializer.is_valid():
+            # we can not change initial start date of survey
+            data = serializer.validated_data
+            new_date = data['start_date']
+            if survey.start_date != new_date:
+                return Response({'status': 'Changing start date is not allowed'})
+            else:
+                serializer.update(survey, data)
+                return Response({'status': 'OK'})
+        else:
+            return Response(serializer.errors,
+                            status=status.HTTP_400_BAD_REQUEST)
+
+
+    def destroy(self, request, pk):
+        survey = get_object_or_404(Survey.objects.all(), id=pk)
+        survey.delete()
+        return Response({'status': 'OK'})
 
 
 class QuestionTypeViewSet(viewsets.ModelViewSet):
@@ -24,7 +67,7 @@ class QuestionTypeViewSet(viewsets.ModelViewSet):
 
 class QuestionViewSet(viewsets.ModelViewSet):
     """
-    API endpoint that allows Questions to be viewed.
+    API endpoint that allows Questions to be managed
     """
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
